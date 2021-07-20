8d3d743f8de4d0a0167edf8224f593669f8ddcb8
Ryo Suzuki <ryo.not.rio.suzuki@gmail.com>
2021-07-20 16:47:29 +0100

    add duplicate

--- mainpkg/someFunctions.go
+++ mainpkg/someFunctions.go
@@ -7,5 +7,15 @@ func gcd(num1 int, num2 int) int {
 	if num2 == 0 {
 		return num1
 	}
-	return gcd(num2, num1 % num2)
+	return gcd(num2, num1%num2)
+}
+
+func gcd2(num1 int, num2 int) int {
+	if num1 == 0 {
+		return num2
+	}
+	if num2 == 0 {
+		return num1
+	}
+	return gcd(num2, num1%num2)
 }
