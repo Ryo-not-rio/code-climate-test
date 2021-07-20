8d1255d2148ad42e5818c4e46ab6c56e00ced109
Ryo Suzuki <ryo.not.rio.suzuki@gmail.com>
2021-07-20 16:50:51 +0100

    add a bunch of hellooooos

--- mainpkg/someFunctions.go
+++ mainpkg/someFunctions.go
@@ -1,5 +1,7 @@
 package mainpkg
 
+import "fmt"
+
 func gcd(num1 int, num2 int) int {
 	if num1 == 0 {
 		return num2
@@ -11,11 +13,24 @@ func gcd(num1 int, num2 int) int {
 }
 
 func gcd2(num1 int, num2 int) int {
-	if num1 == 0 {
-		return num2
-	}
-	if num2 == 0 {
-		return num1
-	}
-	return gcd(num2, num1%num2)
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
 }
