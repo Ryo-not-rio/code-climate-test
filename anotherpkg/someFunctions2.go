0feb31b88f6f95eed0c04824fc3a0f92ba287907
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2021-08-18 09:31:01 +0100

    add another pkg

---
+++ anotherpkg/someFunctions2.go
@@ -0,0 +1,36 @@
+package anotherpkg
+
+import "fmt"
+
+func gcd(num1 int, num2 int) int {
+	if num1 == 0 {
+		return num2
+	}
+	if num2 == 0 {
+		return num1
+	}
+	return gcd(num2, num1%num2)
+}
+
+func gcd2(num1 int, num2 int) int {
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+	fmt.Println("HElloooo")
+}
